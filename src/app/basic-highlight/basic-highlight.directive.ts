80fc3dadb4fd14ed6606d800ce0fb2e8f7b70633
anhdo <anh.dinh@savvycomsoftware.com>
2020-04-16 22:42:52 +0700

    Binding directive properties

--- src/app/basic-highlight/basic-highlight.directive.ts
+++ src/app/basic-highlight/basic-highlight.directive.ts
@@ -1,24 +1,29 @@
-import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding } from '@angular/core';
+import { Directive, ElementRef, Renderer2, OnInit, HostListener, HostBinding, Input } from '@angular/core';
 
 @Directive({
   selector: '[appBasicHighlight]'
 })
 export class BasicHighlightDirective implements OnInit {
+  @Input('appBasicHighlight') defaultColor: string
+  @Input() defaultBgColor: string = 'transparent'
+
+  @HostBinding('style.color') color: string
   @HostBinding('style.backgroundColor') bgColor: string
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+    this.color = this.defaultColor
+    this.bgColor = this.defaultBgColor
   }
 
   @HostListener('mouseenter') mouseenter() {
-    this.bgColor = '#d2d2e3'
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', '#333')
+    this.bgColor = "#aa2233"
+    this.color = "#fff"
   }
 
   @HostListener('mouseleave') mouseleave() {
-    this.bgColor = 'transparent'
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
-    // this.renderer.setStyle(this.elementRef.nativeElement, 'color', 'inherit')
+    this.color = this.defaultColor
+    this.bgColor = this.defaultBgColor
   }
 }
