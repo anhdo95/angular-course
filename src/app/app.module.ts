39a67dc29f7a01cb96dbced9a85b2cf43e9b01fa
anhdo <anh.dinh@savvycomsoftware.com>
2020-04-16 23:16:28 +0700

    Build a custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeItemComponent } from './recipes/recipe-item/recipe-item.component';
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { UnlessDirective } from '../shared/directives/unless.directive';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { BasicHighlightDirective } from './basic-highlight/basic-highlight.direc
     RecipeItemComponent,
     RecipeDetailComponent,
     BasicHighlightDirective,
+    UnlessDirective,
   ],
   imports: [
     BrowserModule,
